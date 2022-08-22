497edefe06a2dcfa3b8c78c6197cbc8be3d65842
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2022-08-22 02:04:37 +0200

    Clean up config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-	extends: [
-		'next/core-web-vitals',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-	],
+	extends: ['next', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['@typescript-eslint'],
 	settings: {
 		next: {
@@ -13,9 +9,9 @@ module.exports = {
 	root: true,
 	ignorePatterns: [
 		'node_modules/*',
-		'.next',
 		'build/',
 		'dist/',
+		'.next/',
 		'.cache/',
 		'.tmp/',
 	],
