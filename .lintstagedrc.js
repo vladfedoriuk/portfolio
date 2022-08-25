606888883390c6de79f24b0a415bb05f8cbbd542
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2022-08-25 23:21:52 +0200

    Adjust lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-	// Type check TypeScript files
-	'apps/front/{,**/}*.(ts|tsx)': () =>
-		'yarn tsc --noEmit --project apps/front/tsconfig.json',
-	'apps/cms/{,**/}*.(ts|tsx)': () =>
-		'yarn tsc --noEmit --project apps/cms/tsconfig.json',
+  // Type check TypeScript files
+  "apps/front/{,**/}*.(ts|tsx)": () =>
+    "yarn tsc --noEmit --project apps/front/tsconfig.json",
+  "apps/cms/{,**/}*.(ts|tsx)": () =>
+    "yarn tsc --noEmit --project apps/cms/tsconfig.json",
 
-	// Lint then format TypeScript and JavaScript files
-	'**/*.(ts|tsx|js)': filenames => [
-		`yarn eslint --fix ${filenames.join(' ')}`,
-		`yarn prettier --write ${filenames.join(' ')}`,
-	],
+  // Lint then format TypeScript and JavaScript files
+  "{,**/}*.(ts|tsx|js)": (filenames) => [
+    `yarn eslint --fix ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
 
-	// Format MarkDown and JSON
-	'**/*.(md|json)': filenames =>
-		`yarn prettier --write ${filenames.join(' ')}`,
+  // Format MarkDown and JSON
+  "{,**/}*.(md|json)": (filenames) =>
+    `yarn prettier --write ${filenames.join(" ")}`,
 };
