255654a2097dc9d537285db8df9749f9123ec058
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2022-08-25 23:15:50 +0200

    Add material ui setup for front app

--- apps/front/pages/_app.tsx
+++ apps/front/pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from 'theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@components/AppBar';
+import createEmotionCache from '@utils/cache/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
+import Head from 'next/head';
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+function MyApp({
+	Component,
+	emotionCache = clientSideEmotionCache,
+	pageProps,
+}: MyAppProps) {
+	return (
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<AppBar />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	);
 }
 
 export default MyApp;
---
+++ apps/front/types/app.d.ts
@@ -0,0 +1,8 @@
+import { EmotionCache } from '@emotion/react';
+import type { AppProps } from 'next/app';
+
+declare global {
+	interface MyAppProps extends AppProps {
+		emotionCache?: EmotionCache;
+	}
+}
