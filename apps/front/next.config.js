d6258aade30fc0ce59b9eef2fb3b2b0e02320b3b
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2022-11-27 17:19:17 +0100

    Use SWC instead of Babel

--- apps/front/next.config.js
+++ apps/front/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
